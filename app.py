e6811c1bee2b7f31415bf914393925572d8836cc
murlocbeats <murlocbeats2@gmail.com>
2024-08-02 19:54:28 +0330

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
 from flask import Flask
+import hmac
+import hashlib
+import base64
+import requests
+import time
+import json
+import random
+import string
+
+# Your API key and secret
+API_KEY = 'cc_59c730b0f45a7f8f9d7a43483cf09cb1'
+API_SECRET = '73b3ad0e84916ce9103c3582d168e88bf1b7cf99376290d0992904c60ab86d73'
+API_PASSPHRASE = '13731373'  # Replace with your actual passphrase
+
+# Generate the current timestamp in milliseconds
+timestamp = int(time.time() * 1000)
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'Hiiiiii'
+    return timestamp
 
 if __name__ == '__main__':
     app.run(debug=True)
