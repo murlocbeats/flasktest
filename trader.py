1abf2a55941c816689d8ff40cf5d6768dd08159c
murlocbeats <murlocbeats2@gmail.com>
2024-08-02 22:12:35 +0330

    Update trader.py

--- trader.py
+++ trader.py
@@ -6,7 +6,7 @@ import time
 import json
 import random
 import string
-from telegram_sender import *
+#from telegram_sender import *
 
 # Your API key and secret
 API_KEY = 'cc_59c730b0f45a7f8f9d7a43483cf09cb1'
